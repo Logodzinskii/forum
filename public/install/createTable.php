9821c1ea7632491781d3a750b9212545fa286776
Aleksandr Logodzinsky <ekasaitlim@gmail.com>
2024-08-08 21:13:14 +0500

    fix key in table

--- public/install/createTable.php
+++ public/install/createTable.php
@@ -11,13 +11,14 @@ class createTable {
     public function createUsers() : void {
 
         $table = 'users';
-        $sql ="CREATE table $table(
+        $sql ="CREATE TABLE IF NOT EXISTS $table(
             ID INT( 11 ) AUTO_INCREMENT PRIMARY KEY,
             user_name VARCHAR( 250 ) NOT NULL,
-            user_email VARCHAR( 250 ) NOT NULL,
+            user_email VARCHAR( 250 ) NOT NULL UNIQUE,
             user_status VARCHAR( 150 ) NOT NULL,
             created_at TIMESTAMP NOT NULL,
-            updated_at TIMESTAMP NOT NULL);";
+            updated_at TIMESTAMP NOT NULL
+           );";
 
         echo $this->createTable($table, $sql);
                
@@ -30,13 +31,18 @@ class createTable {
     public function create_Topic() : void {
         
         $table = 'topics';
-        $sql ="CREATE table $table(
+        $sql ="CREATE TABLE IF NOT EXISTS $table(
             ID INT( 11 ) AUTO_INCREMENT PRIMARY KEY,
-            user_id INT( 250 ) NOT NULL,
+            user_id INT,
+            INDEX par_ind (user_id),
+                FOREIGN KEY (user_id)
+                REFERENCES users(id)
+                ON DELETE CASCADE,
             topic_name VARCHAR( 250 ) NOT NULL,
             topic_descryption VARCHAR( 250 ) NOT NULL,
             created_at TIMESTAMP NOT NULL,
-            updated_at TIMESTAMP NOT NULL);";
+            updated_at TIMESTAMP NOT NULL
+            );";
         
         echo $this->createTable($table, $sql);
     }
@@ -48,10 +54,18 @@ class createTable {
     public function create_Messages() : void {
         
         $table = 'messages';
-        $sql ="CREATE table $table(
+        $sql ="CREATE TABLE IF NOT EXISTS $table(
             ID INT( 11 ) AUTO_INCREMENT PRIMARY KEY,
             user_id INT( 250 ) NOT NULL,
+            INDEX use_ind (user_id),
+                FOREIGN KEY (user_id)
+                REFERENCES users(id)
+                ON DELETE CASCADE,
             topic_id INT( 250 ) NOT NULL,
+            INDEX top_ind (topic_id),
+                FOREIGN KEY (topic_id)
+                REFERENCES topics(id)
+                ON DELETE CASCADE,
             user_message TEXT NOT NULL,
             created_at TIMESTAMP NOT NULL,
             updated_at TIMESTAMP NOT NULL);";
