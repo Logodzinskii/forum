ca038608946e97ba6a7649ba1127056c22374b22
Aleksandr Logodzinsky <ekasaitlim@gmail.com>
2024-08-08 23:39:52 +0500

    create topic view

--- public/index.php
+++ public/index.php
@@ -4,28 +4,55 @@
 ?>
 <body>
    <?
+
     $topics = new TopickControllers();
 
-    foreach($topics->getTopic() as $topic)
+    $topicGet = isset($_GET['topic']) ? $_GET['topic'] : '';
+    if($topicGet != '')
+    {
+        foreach($topics->getTopicMessage($_GET['topic']) as $topic)
+                {
+                    if( !isset($topic['noTopic']) ){
+
+                        echo '<div class="topic">';
+                        echo '<div class="topic-name"><a href="/?topic='.$topic['topic_id'].'">'.$topic['topic_name'].'</a></div>';
+                        echo '<div class="topic-author">'.$topic['user_name'].'</div>';
+                        echo '<div class="last-author-date">Cообщение создано - '. $topic['messages_created_at'] .'</div>';
+                        echo '</div>';
+
+                    }else{
+
+                        echo $topic['noTopic'];
+
+                    }
+                    
+                }; 
+                echo '<a href="components/form_topic.php" class="a-button">Создать сообщение</a> ';
+    }else{
+        foreach($topics->getTopic() as $topic)
         {
             if( !isset($topic['noTopic']) ){
 
                 echo '<div class="topic">';
-                echo '<div class="topic-name"><a href="#">'.$topic['topic_name'].'</a></div>';
-                echo '<div class="topic-author">'.$topic['topic_name'].'</div>';
-                echo '<div class="inner-topic-message-count">'.$topic['topic_name'].'</div>';
-                echo '<div class="last-author">'.$topic['topic_name'].'</div>';
+                echo '<div class="topic-name"><a href="/?topic='.$topic['topic_id'].'">'.$topic['topic_name'].'</a></div>';
+                echo '<div class="topic-author">'.$topic['user_name'].'</div>';
+                echo '<div class="inner-topic-message-count">'.$topic['count_message'].'</div>';
+                echo '<div class="last-author">Последнее сообщение от автора - '. $topic['last_user_name'] .'</div>';
+                echo '<div class="last-author-date">Последнее сообщение создано - '. $topic['messages_created_at'] .'</div>';
                 echo '</div>';
-
+                
             }else{
 
                 echo $topic['noTopic'];
 
             }
             
-        };  
+        }; 
+        echo '<a href="components/form_topic.php" class="a-button">Создать тему</a>';
+    }
+ 
    ?>
-   <a href="components/form_topic.php" class="a-button">Создать тему</a>
+   
    <div class="pagination"></div>
 </body>
 <?
